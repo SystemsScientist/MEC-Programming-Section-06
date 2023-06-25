d6dcdc4576389b77aa5db44dd826c94f94ff3233
Matt Johnson <matthewjohnson@snarkydata.com>
2023-06-24 17:57:33 -0700

    Adding 47-ASCII-Codes files and directories

---
+++ 47-ASCII-Codes/main.c
@@ -0,0 +1,28 @@
+/*
+    Author: Matt Johnson
+    Subject: ascii codes
+    Purpose:
+    Date: 06/24/2023
+
+    Notes:
+
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+
+    char a1 = 'A'; // ASCII code 65
+    char a2 = 'p'; // ASCII code 112
+    char a3 = 'p'; // ASCII code 112
+    char a4 = 'l'; // ASCII code 108
+    char a5 = 'e'; // ASCII code 101
+    char a6 = ':'; // ASCII code 58
+    char a7 = ')'; // ASCII code 41
+
+    printf("The string is: %c%c%c%c%c%c%c\n", a1, a2, a3, a4, a5, a6, a7);
+
+    return 0;
+}
